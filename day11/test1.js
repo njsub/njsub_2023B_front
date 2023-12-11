c2a6d00c4268db98ac40ae6cef9df5bea0be5a2e
[github <[github>
2023-12-11 18:58:58 +0900

    commit

--- day11/test1.js
+++ day11/test1.js
@@ -1,23 +1,36 @@
 
-
-const a = Number(prompt('속도 값을 입력해주세요'))
-if(a==0){console.log('시동이 꺼진다');}
-else if(a>0){console.log('시동이 켜진다');}
-
-const b = Number(prompt ('속도를 입력하세요'))
-if(b==0){console.log('시동이 꺼진다');}
-
-const c = Number(prompt('숫자를 입력하세요'))
-if(c>=0){console.log('양수입니다.');}
-else {console.log('음수입니다.');}
+/*  
 
 
-function 문제4함수(){
+*/
 
-//입력
-let value1 = Number(document.querySelector)('#input1').value); console.log(value1)
+//문제1 : 3개 점수를 각 input으로 입력받아 총점과 평균 span에 출력하시오.
+function 연습버튼클릭(){100
 
-//처리
+    //입력
+    let value1 = document.querySelector('#input1').value;
+    let value2 = document.querySelector('#input2').value;
+    let value3 = document.querySelector('#input3').value;
 
-//출력
+    // 처리
+    let sumValue = Number(value1)+Number(value2)+Number(value3);
+    let avgValue = sumValue/3;
+    
+    // 출력
+    document.querySelector('#result').innerHTML
+                = ` 총점 : ${sumValue}점 평균:${avgValue}점`;
+}
+
+function 문제함수1(){
+    
+    //입력
+    let val1 = document.querySelector('#input4').value;
+    let val2 = document.querySelector('#input5').value;
+    let val3 = document.querySelector('#input6').value;
+    //처리
+    let sumValue1 = Number(val1)+Number(val2)+Number(val3);
+    let avgValue1 = sumValue1/3;
+    //출력
+    document.querySelector('#abv').innerHTML
+                = ` 총점 : ${sumValue1}점 평균 : ${avgValue1}점`;
 }
