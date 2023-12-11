300d0dd8bf291ad1be8dac35904b402e74460544
njsub <njsub@naver.com>
2023-12-11 09:56:40 +0900

    [20231211 09:56] day11 수정

--- day11/step1.js
+++ day11/step1.js
@@ -67,7 +67,43 @@ function 변환기능(){
 
 
 
-//예제3
+// 앞으로의 웹개발 방향
+function 변환기능코드가지고있는상자(){ 
+    const hp = document.querySelector('input');
+    console.log( 인치 );  console.log( 인치.value );
+        // document : HTML 뜻   // query : 질의 / 요청   // Selector : 선택
+            // document.querySelector( "마크업" vs ".class명" , "#id명" ) : HTML에 요소 선택.
+            // HTML 요소를 JS의 변수/객체 로 가져오기 함수
+    // ---------> 처리 ------> java에게 통신으로 보내기 
+    // 결과 = input에 입력받은 value를 h3[ innerHTML=마크업 사이에 ]에 대입 
+    document.querySelector('h3').innerHTML = `${ 인치.value * 2.54 }cm`
+} // 함수 종료되면 모두 코드/변수 다 사라짐.
+    // 선언 약속/규칙/문법 : 변수 = let , 상수 = const , 함수 = function     
+    // function 함수명( 매개변수 ){ 구현할기능코드 }    
+
+ /*   - 각 생활에서 사용되는 기계/프로그램들 예시 만들기 [예시6번] - 4/5명
+    - 1.키오스크 , 2.엘리베이터  , 3.자율주행 자동차
+    예) 편의점 라면 포트기.
+    1. 저장/변수         5개이상 :  온도 , 면종류 , 시간 , 온수상태 , 완료상태
+    2. 기능/함수         5개이상 : 시작 , 물추가 , 상태변경 , 조리시간추가 , 면종류 선택
+    3. 경우의수/논리     5개이상 : 용기있다/없다, 조리시간 완료/조리중 , 상태 점검중/사용가능중 , 온수 부족/충분 , 온도 최대값 이상/이하
+
+    4. 1~3 시각화 그림으로 표현
+
+
+*/
+
+/* 3 */
 const cm정수 = prompt("cm 단위의 숫자를 입력해주세요.");
 const 인치 = Number(cm정수) / 2.54;
 alert(`${cm정수}cm는 ${인치}inch 입니다.`);
+
+/* 4 */
+let 반지름 = Number(prompt('원의 반지름을 입력해주세요.'));
+console.log(` 원의 넓이 : ${3.14 * 반지름 * 반지름} / 원의 둘레 : ${2 * 3.14 * 반지름} `);
+
+/* 5 */
+const 금액 = prompt("달러 단위의 금액을 입력하세요");
+const 달러 = Number(금액);
+const 원화 = 달러*1207;
+console.log(`달러 : ${달러} -> 원화 : ${원화}`)
--- day11/step3.js
+++ day11/step3.js
@@ -79,3 +79,27 @@ document.querySelector('#result').innerHTML
 
 }
 
+/*문제 5 : 점수[입력값1]를 입력받아 점수 90점이상 A등급 출력 
+                            80점이상 B등급 출력 
+                            70점이상 C등급 출력  
+                            그외 재시험*/
+
+function 문제5함수(){ // f s     
+    //[입력]
+    let value1 = Number(document.querySelector('#input1').value);  
+    //[처리]
+    let result = '';
+        // 처리 로직 
+        if( value1 >= 90 ){ result = 'A등급'; }
+        else if( value1 >= 80 ){ result = 'B등급';}
+        else if( value1 >= 70 ){ result = 'C등급';}
+        else{ result = '재시험'; }
+    //[출력]
+    document.querySelector('#result').innerHTML = `${ result }`;
+} // f e 
+
+/*문제 6: 아이디[입력값1] 와 비밀번호[입력값2] 입력받기 
+             		회원아이디가 admin 이고 비밀번호가 1234 이면 로그인 성공 출력 아니면 로그인실패 출력
+*/
+
+//문제 7 : 3개의 정수[입력값1/2/3] 를 입력받아서 오름차순 / 내림차순 출력 
