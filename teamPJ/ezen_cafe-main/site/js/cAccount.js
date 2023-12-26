ac77c3e7d79003d6afae261df65dd4e39c83d323
[github <[github>
2023-12-26 18:20:00 +0900

    20231226 오후 퇴근

---
+++ teamPJ/ezen_cafe-main/site/js/cAccount.js
@@ -0,0 +1,15 @@
+console.log('cAccount 회원가입 페이지 실행')
+
+function _signup2(){
+    console.log('cAccount 회원가입 버튼 실행')
+    const ezenId = document.querySelector('#ezenId').value;
+    const ezenPw = document.querySelector('#ezenPw').value;
+    const ezenPwCheck = document.querySelector('#ezenPwcheck').value;
+    console.log(ezenId); console.log(ezenPw); console.log(ezenPwCheck);
+    if(ezenPw !== ezenPwCheck){ alert('비밀번호/비밀번호 확인이 다릅니다!'); return;}
+
+    
+
+    const 
+
+}
---
+++ teamPJ/ezen_cafe-main/site/js/main.js
@@ -0,0 +1,13 @@
+console.log('main js 실행')
+
+function _cAccount(){
+    console.log('cAccount 펑션, 회원가입 사이트 실행')
+    location.href='html/cAccount.html'
+}
+
+function _singup(){
+    console.log('로그인 펑션 실행')
+    const mId = prompt('아이디를 입력하세요.')
+    const mPassword = prompt('비밀번호를 입력하세요.')
+
+}
